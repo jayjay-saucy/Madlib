9d4e6b886158b12fa6e912897e701eaeb6e6d510
Jayllian Marquardt <jayllian.marquardt.2021@student.lhschools.org>
2020-03-02 13:58:13 -0500

    MadLib commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,222 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       // Phase 1 - 20 variables
+        String name;
+        String greeting;
+        String activity;
+        String typeOfFood;
+        String shockingResponse;
+        String evilPlan;
+        String famousPersonsName;
+        String adjective;
+        String emotion;
+        String typeOfMotion;
+        String funnyWord;
+        String weirdName;
+        String number;
+        String typeOfDrink;
+        String answerIGive;
+        String bodyPart;
+        String angryEmotion;
+        String funnyName;
+        String superpower;
+        String superWeapon;
+        String superheroName;
+        String typeOfScaryPlace;
+
+
+
+
+
+
+
+        Scanner bow;
+
+
+                bow = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+//        Phase 2 - prompting for input and storing
+
+
+        System.out.println("Give me a greeting word");
+        greeting = bow.nextLine();
+
+        System.out.println("I have a story to tell... what are you doing right now?");
+        activity = bow.nextLine();
+
+        System.out.println("give me your favorite food");
+        typeOfFood = bow.nextLine();
+
+        System.out.println("give me a shocking answer");
+        shockingResponse = bow.nextLine();
+
+        System.out.println("give me a random name");
+        name = bow.nextLine();
+
+        System.out.println("what is something a villain would try to do");
+        evilPlan = bow.nextLine();
+
+
+        System.out.println("who's your favorite famous person");
+        famousPersonsName = bow.nextLine();
+
+        System.out.println("give me an adjective for example running or jumping");
+        adjective = bow.nextLine();
+
+        System.out.println("same famous person");
+        famousPersonsName = bow.nextLine();
+
+        System.out.println("give me an emotion");
+        emotion = bow.nextLine();
+
+        System.out.println("same adjective");
+        adjective = bow.nextLine();
+
+        System.out.println("give me a funny word");
+        funnyWord = bow.nextLine();
+
+        System.out.println("same type of food");
+        typeOfFood = bow.nextLine();
+
+        System.out.println("give me a type of motion for example running or jumping");
+        typeOfMotion = bow.nextLine();
+
+
+        System.out.println("give me a weird name for a snack");
+        weirdName = bow.nextLine();
+
+        System.out.println("favorite number");
+        number = bow.nextLine();
+
+        System.out.println("favorite drink");
+        typeOfDrink = bow.nextLine();
+
+        System.out.println("same type of food");
+        typeOfFood = bow.nextLine();
+
+        System.out.println("yes or no (choose)");
+        answerIGive = bow.nextLine();
+
+        System.out.println("give me a random body part");
+        bodyPart = bow.nextLine();
+
+        System.out.println("give me an emotion when u don't get your way");
+        angryEmotion = bow.nextLine();
+
+        System.out.println("same type of food as before");
+        typeOfFood = bow.nextLine();
+
+        System.out.println("funny name like in a cartoon ( teen titans)");
+        funnyName = bow.nextLine();
+
+        System.out.println("a power you wish you had");
+        superpower = bow.nextLine();
+
+        System.out.println("a weapon that could beat that power");
+        superWeapon = bow.nextLine();
+
+        System.out.println("favorite superhero");
+        superheroName = bow.nextLine();
+
+        System.out.println("a place you would never want to go to");
+        typeOfScaryPlace = bow.nextLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        Phase 3
+
+
+        System.out.println("Hey "  +  greeting);
+
+        System.out.println("I have a story to tell... what are you doing right now?"  +  activity);
+
+        System.out.println("That seems fun, well listen to this.");
+
+        System.out.println("That seems fun but listen, I had this weird dream that i was eating"  +  typeOfFood);
+
+        System.out.println("and it started talking to me"  +  shockingResponse);
+
+        System.out.println("yea crazy right?! and he told me his name was"  +  name);
+
+        System.out.println("we had a full conversation about how he was going too"  +  evilPlan);
+
+        System.out.println("I was like that's crazy talk, how are you even talking.");
+
+        System.out.println("He said he was created by"  +  famousPersonsName);
+
+        System.out.println("and"  +  adjective);
+
+        System.out.println("away from"  +  famousPersonsName);
+
+        System.out.println("because he was trying to eat him.");
+
+        System.out.println("I was"  +  emotion);
+
+        System.out.println("because i was starving and didn't want him to"  +  adjective);
+
+        System.out.println("away from me.");
+
+        System.out.println("I was looking like a"  +  funnyWord);
+
+        System.out.println("talking to a"  +  typeOfFood);
+
+        System.out.println("so i got up and"  +  typeOfMotion);
+
+        System.out.println(" outside with it. ");
+
+        System.out.println("When i got outside i remembered the party i was at had weird these treats called"  +  weirdName);
+
+        System.out.println("i had eaten like"  +  number);
+
+        System.out.println("i tried drinking some"  +  typeOfDrink);
+
+        System.out.println("but that didn't help, so i asked "  +  name);
+
+        System.out.println("if it was all real or not and he said"  +  answerIGive);
+
+        System.out.println("so at this point i was losing my"  +  bodyPart);
+
+        System.out.println("i started"  +  angryEmotion);
+
+        System.out.println("at the little"  +  typeOfFood);
+
+        System.out.println("after awhile someone called the"  +  funnyName);
+
+        System.out.println("but i wasn't worried i had "  +  superpower + "as powers");
+
+        System.out.println("and i thought i could fight them off but they had"  +  superWeapon + ".");
+
+        System.out.println("I hesitated but still fought them off anyway, i felt like"  +  superheroName);
+
+        System.out.println("but i lost and was put in"  +  typeOfScaryPlace + ".");
+
+        System.out.println("moral of the story watch what you put in your body kids");
+
+
+    }
+}
